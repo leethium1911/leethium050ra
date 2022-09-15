7b4af7fac3fa389c37393b9340d7c05d4d832b97
Lee <Lee@DESKTOP-N3JH7HR>
2022-09-15 13:59:52 +0400

    Добавьте файлы проекта.

---
+++ leethium050ra/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random z = new Random();
+
+            int n = z.Next(51);
+            //Console.WriteLine(n);
+            Console.WriteLine("Угадай рандомное число от 0 до 50. ");
+            Console.WriteLine("Ответ:");
+
+            int x = 1;
+            int y = Convert.ToInt32(Console.ReadLine());
+            while (x <= 3)
+
+            {
+                x++;
+
+                if (y == n)
+                {
+                    Console.WriteLine("Поздравляем вы угадали число с " + x + "попытки");
+                    break;
+                }
+
+
+                else
+                {
+
+
+                    Console.WriteLine("Неверно, попытка номер " + x + ":");
+                    y = Convert.ToInt32(Console.ReadLine());
+
+                    if (x == 3)
+                    {
+                        Console.WriteLine("Вы не угадали число даже с " + x + " попыток, больше не играйте!");
+                        break;
+                    }
+
+                }
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
